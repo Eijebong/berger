8c58b0f896e480c63ea27b7b67b6c17460608f44
Bastien Orivel <eijebong@bananium.fr>
2022-03-11 19:02:59 +0100

    Refactor auth handlers out of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 mod db;
 mod models;
 mod pulse;
+mod views;
 
 use anyhow::Result;
 use models::TaskGroup;
-use oauth2::basic::BasicClient;
-use oauth2::{AuthUrl, ClientId, CsrfToken, RedirectUrl, Scope};
-use poem::web::Redirect;
+
 use poem::{
     endpoint::StaticFilesEndpoint,
     get, handler,
     listener::TcpListener,
     session::{CookieConfig, CookieSession, Session},
-    web::{cookie::CookieKey, Html, Query},
+    web::{cookie::CookieKey, Html},
     EndpointExt, Route, Server,
 };
-use reqwest::Url;
+
 use std::collections::HashSet;
 
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use taskcluster::Credentials;
 use tera::{Context, Tera};
 
@@ -55,87 +54,6 @@ struct ComputedTaskGroup<'a> {
     status: &'a str,
     start: &'a str,
     end: &'a str,
-}
-
-#[handler]
-async fn logout(session: &Session) -> Result<Redirect> {
-    session.remove("credentials");
-    Ok(Redirect::see_other("/"))
-}
-
-#[handler]
-async fn login() -> Result<Redirect> {
-    let client = BasicClient::new(
-        ClientId::new("berger".to_string()),
-        None,
-        AuthUrl::new(format!("{}/login/oauth/authorize", &*BASE_URL))?,
-        None,
-    )
-    .set_redirect_uri(RedirectUrl::new(
-        std::env::var("REDIRECT_URL").unwrap_or_else(|_| {
-            let url = Url::parse(&**BASE_URL).unwrap();
-            format!("https://berger.{}/auth/callback", url.host().unwrap())
-        }),
-    )?);
-
-    let (url, _) = client
-        .authorize_url(CsrfToken::new_random)
-        .add_scope(Scope::new("berger:*".into()))
-        .url();
-
-    Ok(Redirect::see_other(url))
-}
-
-#[derive(Deserialize)]
-struct Callback {
-    code: String,
-}
-
-#[handler]
-async fn callback(
-    Query(Callback { code }): Query<Callback>,
-    session: &Session,
-) -> Result<Redirect> {
-    let client = reqwest::Client::new();
-    let req = client
-        .post(format!("{}/login/oauth/token", &*BASE_URL))
-        .form(&[
-            ("code", code),
-            ("grant_type", "authorization_code".into()),
-            ("client_id", "berger".into()),
-            (
-                "redirect_uri",
-                std::env::var("REDIRECT_URL").unwrap_or_else(|_| {
-                    let url = Url::parse(&**BASE_URL).unwrap();
-                    format!("https://berger.{}/auth/callback", url.host().unwrap())
-                }),
-            ),
-        ])
-        .build()?;
-    let res = client.execute(req).await?;
-    let body: serde_json::Value = res.json().await?;
-    if let Some(token) = body["access_token"].as_str() {
-        if let Ok(creds) = fetch_creds(token).await {
-            session.set("credentials", creds);
-            Ok(Redirect::see_other("/"))
-        } else {
-            Ok(Redirect::see_other("/?invalid-creds"))
-        }
-    } else {
-        Ok(Redirect::see_other("/?invalid-auth"))
-    }
-}
-
-async fn fetch_creds(token: &str) -> Result<Credentials> {
-    let client = reqwest::Client::new();
-    let req = client
-        .get(format!("{}/login/oauth/credentials", &*BASE_URL))
-        .header("Authorization", format!("Bearer {}", token))
-        .header("Content-Type", "application/json")
-        .build()?;
-    let res = client.execute(req).await?;
-    let creds = &res.json::<serde_json::Value>().await?["credentials"];
-    Ok(serde_json::from_value(creds.clone())?)
 }
 
 #[handler]
@@ -277,9 +195,9 @@ async fn main() -> Result<()> {
 
     let app = Route::new()
         .at("/", get(root))
-        .at("/auth/callback", get(callback))
-        .at("/auth/login", get(login))
-        .at("/auth/logout", get(logout))
+        .at("/auth/callback", get(views::auth::callback))
+        .at("/auth/login", get(views::auth::login))
+        .at("/auth/logout", get(views::auth::logout))
         .nest(
             "/static",
             StaticFilesEndpoint::new("./static").show_files_listing(),
---
+++ src/views/auth.rs
@@ -0,0 +1,86 @@
+use crate::BASE_URL;
+use anyhow::Result;
+use oauth2::basic::BasicClient;
+use oauth2::{AuthUrl, ClientId, CsrfToken, RedirectUrl, Scope};
+use poem::web::Redirect;
+use poem::{handler, session::Session, web::Query};
+use reqwest::Url;
+use serde::Deserialize;
+use taskcluster::Credentials;
+
+lazy_static::lazy_static!(
+    pub static ref REDIRECT_URL: String = std::env::var("REDIRECT_URL").unwrap_or_else(|_| {
+        let url = Url::parse(&**BASE_URL).unwrap();
+        format!("https://berger.{}/auth/callback", url.host().unwrap())
+    });
+);
+
+#[derive(Deserialize)]
+pub struct Callback {
+    code: String,
+}
+
+#[handler]
+pub async fn logout(session: &Session) -> Result<Redirect> {
+    session.remove("credentials");
+    Ok(Redirect::see_other("/"))
+}
+
+#[handler]
+pub async fn callback(
+    Query(Callback { code }): Query<Callback>,
+    session: &Session,
+) -> Result<Redirect> {
+    let client = reqwest::Client::new();
+    let req = client
+        .post(format!("{}/login/oauth/token", &*BASE_URL))
+        .form(&[
+            ("code", code),
+            ("grant_type", "authorization_code".into()),
+            ("client_id", "berger".into()),
+            ("redirect_uri", REDIRECT_URL.clone()),
+        ])
+        .build()?;
+    let res = client.execute(req).await?;
+    let body: serde_json::Value = res.json().await?;
+    if let Some(token) = body["access_token"].as_str() {
+        if let Ok(creds) = fetch_creds(token).await {
+            session.set("credentials", creds);
+            Ok(Redirect::see_other("/"))
+        } else {
+            Ok(Redirect::see_other("/?invalid-creds"))
+        }
+    } else {
+        Ok(Redirect::see_other("/?invalid-auth"))
+    }
+}
+
+pub async fn fetch_creds(token: &str) -> Result<Credentials> {
+    let client = reqwest::Client::new();
+    let req = client
+        .get(format!("{}/login/oauth/credentials", &*BASE_URL))
+        .header("Authorization", format!("Bearer {}", token))
+        .header("Content-Type", "application/json")
+        .build()?;
+    let res = client.execute(req).await?;
+    let creds = &res.json::<serde_json::Value>().await?["credentials"];
+    Ok(serde_json::from_value(creds.clone())?)
+}
+
+#[handler]
+pub async fn login() -> Result<Redirect> {
+    let client = BasicClient::new(
+        ClientId::new("berger".to_string()),
+        None,
+        AuthUrl::new(format!("{}/login/oauth/authorize", &*BASE_URL))?,
+        None,
+    )
+    .set_redirect_uri(RedirectUrl::new(REDIRECT_URL.clone())?);
+
+    let (url, _) = client
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new("berger:*".into()))
+        .url();
+
+    Ok(Redirect::see_other(url))
+}
